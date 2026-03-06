1b94e922175abcb59368d79a55d4453bfdd64fcd
kilito <wudalong@3h1china.com>
2026-03-06 20:04:56 +0800

    创建模块

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from '../api/api.module';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
