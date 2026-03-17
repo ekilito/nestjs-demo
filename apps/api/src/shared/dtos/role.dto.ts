12ef30890b856347c8db1603157361733087ad0b
kilito <wudalong@3h1china.com>
2026-03-17 17:11:05 +0800

    角色管理 entity dto

---
+++ apps/api/src/shared/dtos/role.dto.ts
@@ -0,0 +1,38 @@
+import { ApiProperty, PartialType as PartialTypeFromSwagger } from '@nestjs/swagger';
+import { IsString, IsOptional, IsNumber } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { Type } from 'class-transformer';
+
+export class CreateRoleDto {
+  @IsString()
+  @ApiProperty({ description: '名称', example: 'name' })
+  name: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  @ApiProperty({ description: '状态', example: 1 })
+  status: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  @ApiProperty({ description: '排序号', example: 100 })
+  sort: number;
+}
+
+export class UpdateRoleDto extends PartialTypeFromSwagger(PartialType(CreateRoleDto)) {
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty({ description: 'ID', example: 1 })
+  id: number;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: '用户名', example: 'nick' })
+  username: string;
+
+  @ApiProperty({ description: '密码', example: '666666' })
+  @IsOptional()
+  password: string;
+}
