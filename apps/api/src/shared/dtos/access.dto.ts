19e509759c7d6f292eb869bb67e047e14b6355c3
kilito <wudalong@3h1china.com>
2026-03-18 16:06:59 +0800

    entity dto

--- apps/api/src/shared/dtos/access.dto.ts
+++ apps/api/src/shared/dtos/access.dto.ts
@@ -1,25 +1,50 @@
 import { ApiProperty, ApiPropertyOptional, PartialType as PartialTypeFromSwagger } from '@nestjs/swagger';
-import { IsString, IsOptional, IsNumber } from 'class-validator';
+import { IsString, IsOptional, IsNumber, IsEnum } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { Type } from 'class-transformer';
 import { PageDto } from './page.dto';
+
+export enum AccessType {
+  MODULE = 'module',
+  MENU = 'menu',
+  FEATURE = 'feature',
+}
 
 export class CreateAccessDto {
   @IsString()
   @ApiProperty({ description: '资源名称', example: 'accessName' })
   accessName: string;
 
+  @ApiProperty({ description: '类型', example: 'module' }) // 类型，module、menu、feature
+  @IsEnum(AccessType)
+  type?: AccessType;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: 'url地址', example: 'admin/users' })
+  url?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: 'parentID', example: '12345678901234567890' })
+  parentId?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: '描述', example: 'accessDescription' })
+  description?: string;
+
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
   @ApiProperty({ description: '状态', example: 1 })
-  status: number;
+  status?: number;
 
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
   @ApiProperty({ description: '排序号', example: 100, required: false })
-  sort: number;
+  sort?: number;
 }
 
 export class UpdateAccessDto extends PartialTypeFromSwagger(CreateAccessDto) {
--- apps/api/src/shared/entities/access.entity.ts
+++ apps/api/src/shared/entities/access.entity.ts
@@ -6,10 +6,16 @@ import {
   UpdateDateColumn,
   PrimaryColumn,
   BeforeInsert,
+  TreeChildren,
+  TreeParent,
+  Tree,
 } from 'typeorm';
 import { generateSnowflakeId } from '../utils/snowflake';
+import { AccessType } from '../dtos/access.dto';
+
 
 @Entity()
+@Tree("materialized-path") // materialized-path 表示使用物化路径存储树结构
 export class Access {
   @PrimaryColumn({ type: 'bigint' })
   @ApiProperty({ description: 'ID', example: '12345678901234567890' })
@@ -25,6 +31,27 @@ export class Access {
   @Column({ length: 50, unique: true })
   @ApiProperty({ description: '资源名称', example: 'accessName' })
   accessName: string;
+
+  @Column({ type: 'enum', enum: AccessType, default: AccessType.MODULE })
+  type: AccessType;
+
+  @Column({ length: 200, nullable: true }) // nullable 表示该字段可以为空
+  @ApiProperty({ description: '资源URL', example: '/api/access' })
+  url: string;
+
+  @Column({ length: 200, nullable: true })
+  @ApiProperty({ description: '资源描述', example: 'accessDescription' })
+  description: string;
+
+  @TreeChildren()
+  @ApiProperty({ description: '子资源', type: () => [Access] })
+  children: Access[];
+
+  // 👉 ❌ 不要在 Entity 里加 parentId 字段
+  // 👉 ✅ 只保留 parent: Access 就够了 这是 TypeORM 树结构模型，它内部会自动帮你维护：parent: Access 本质上就已经包含了 parentId
+  @TreeParent() // TreeParent 表示该字段是树结构的父节点
+  @ApiProperty({ description: '父资源', type: () => Access })
+  parent: Access;
 
   @Column({ default: 1 })
   @ApiProperty({ description: '生效状态', example: 1 })
