7c07e249e5804c8b30d147fa3db73033fbab05c4
kilito <wudalong@3h1china.com>
2026-03-18 16:13:11 +0800

    access dto

--- apps/api/src/shared/dtos/access.dto.ts
+++ apps/api/src/shared/dtos/access.dto.ts
@@ -17,7 +17,7 @@ export class CreateAccessDto {
 
   @ApiProperty({ description: '类型', example: 'module' }) // 类型，module、menu、feature
   @IsEnum(AccessType)
-  type?: AccessType;
+  type: AccessType;
 
   @IsString()
   @IsOptional()
@@ -38,13 +38,13 @@ export class CreateAccessDto {
   @IsOptional()
   @Type(() => Number)
   @ApiProperty({ description: '状态', example: 1 })
-  status?: number;
+  status: number;
 
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
   @ApiProperty({ description: '排序号', example: 100, required: false })
-  sort?: number;
+  sort: number;
 }
 
 export class UpdateAccessDto extends PartialTypeFromSwagger(CreateAccessDto) {
@@ -57,5 +57,16 @@ export class AccessPageDto extends PageDto {
   @IsString()
   @IsOptional()
   @ApiPropertyOptional({ description: '资源名称', example: 'accessName' })
-  accessName: string;
+  accessName?: string;
 }
+
+// Create DTO：
+//   必填 → 不加 ? + ApiProperty
+//   可选 → ? + IsOptional + ApiPropertyOptional
+
+// Update DTO：
+//   直接 PartialType
+
+// Page DTO：
+//   全部可选（? + IsOptional）
+//   并在 Swagger 中标记为可选（@ApiPropertyOptional）
